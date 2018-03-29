3dce9fa898ad5202104c8ca3b4b600bf65349a3e
twknab <20636750+twknab@users.noreply.github.com>
2018-03-29 16:45:29 -0700

    added pt 2

---
+++ algorithms_4/Week 2/wk2_trees_part_ii.js
@@ -0,0 +1,14 @@
+function BTNode(value) {
+  this.val = value;
+  this.left = null;
+  this.right = null;
+}
+
+function BST() {
+ this.root = null;
+}
+
+console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+console.log("$$$$$$$$$         TEST        $$$$$$$$");
+console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+// let's test our functions:
